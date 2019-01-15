64fc4c9f580e05d91f88b354747a63c029889e10
gusttavo212 <gusttavo212@gmail.com>
2019-01-15 10:33:01 -0200

    Fnish the CRUD and export api using cors

--- src/controller/CardController.js
+++ src/controller/CardController.js
@@ -9,10 +9,25 @@ module.exports = {
 
         return res.json(cards);
     },
-
+    async show(req, res) {
+        const card = await Card.findById(req.params.id);
+        return res.json(card)        
+    },
     async create (req, res) {
         const card = await Card.create(req.body);    
         
         return res.json(card);
+    },
+    async update (req, res) {
+        const card = await Card.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        });
+
+        return res.json(card);
+    },
+    async destroy(req, res){
+        await Card.findByIdAndRemove(req.params.id);
+
+        return res.send();
     }
 }
--- src/models/routes.js
+++ src/models/routes.js
@@ -4,7 +4,9 @@ const routes = express.Router();
 const CardContoller = require('../controller/CardController');
 
 routes.get('/cards', CardContoller.index);
-
+routes.get('/cards/:id', CardContoller.show);
 routes.post('/cards',CardContoller.create);
+routes.put('/cards/:id', CardContoller.update);
+routes.delete('/cards/:id', CardContoller.destroy);
 
 module.exports = routes
